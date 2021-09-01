75567c3db0268d0478dcf8b994c265c91115ce34
pjobling999 <62036028+pjobling999@users.noreply.github.com>
2021-09-01 18:55:10 +0100

    more tunes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,21 @@ export class App extends React.Component {
               <p>
                   <a href="https://drive.google.com/file/d/1npCbEKN2Bf6_mbKr8n1P7_6WEIVTFPsK/view?usp=sharing">SLAMMIN BEATZ 20</a>
               </p>
-      
+              <p>
+                  <a href="https://drive.google.com/file/d/1I3Iq1_jswyItZ2cxh4lgdL7T9W1ucAHW/view?usp=sharing">SLAMMIN BEATZ 19</a>
+              </p>
+              <p>
+                  <a href="https://drive.google.com/file/d/19tKYgXIl0fnWUdaJ9lU2VOsR8nGRCsyc/view?usp=sharing">SLAMMIN BEATZ 18</a>
+              </p>
+              <p>
+                  <a href="https://drive.google.com/file/d/1b_3dgA4TaCwrHzXSer_0LB7PKgeEU_p4/view?usp=sharing">SLAMMIN BEATZ 17</a>
+              </p>
+              <p>
+                  <a href="https://drive.google.com/file/d/1712-tXtSUEN_bbugiFXCHsZh1Poxh7FD/view?usp=sharing">SLAMMIN BEATZ 16</a>
+              </p>
+              <p>
+                  <a href="https://drive.google.com/file/d/1__buFfpr0P0mWgtwhuGmyzfPwbRyu1en/view?usp=sharing">SLAMMIN BEATZ 15</a>
+              </p>
       </div>
 
       <h2>JOBBO SINGLES</h2>
@@ -39,7 +53,12 @@ export class App extends React.Component {
               <p>
                   <a href="https://drive.google.com/file/d/1itBLTZNmNSV8t8b1fOQXezhyAG-Ay6np/view?usp=sharing">THE WOBBLER</a>
               </p>
-              
+              <p>
+                  <a href="https://drive.google.com/file/d/1yUptmSDjUj-aIxt5XfLP5IOpQ5Q8PBNj/view?usp=sharing">BACK TO THE BASICS</a>
+              </p>
+              <p>
+                  <a href="https://drive.google.com/file/d/1WgTpw9XFDrddQsz6fuAzd9j51jBx2CCT/view?usp=sharing">KEY - FREE</a>
+              </p>
       </div>
     </div>
   )
