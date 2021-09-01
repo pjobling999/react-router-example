3e9940a6a6f8026a103b0be93f74755646a6e9f9
pjobling999 <62036028+pjobling999@users.noreply.github.com>
2021-09-01 17:29:41 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export class App extends React.Component {
+  render() { return (
+    <div>
+      <h2>JOBBO MIXES</h2>
+      <div style={{ width: 400 }}>
+      
+              <p>
+                  <a href="https://drive.google.com/file/d/14CvBVPxKCiVOHLhYWg7wArDHpV5rFbQc/view?usp=sharing">SLAMMIN BEATZ 22</a>
+              </p>
+              <p>
+                  <a href="https://drive.google.com/file/d/1ueLS_UlzD-KDc1TzVPrjhuHtcCMagoaN/view?usp=sharing">SLAMMIN BEATZ 21</a>
+              </p>
+              <p>
+                  <a href="https://drive.google.com/file/d/1npCbEKN2Bf6_mbKr8n1P7_6WEIVTFPsK/view?usp=sharing">SLAMMIN BEATZ 20</a>
+              </p>
+      
+      </div>
+
+      <h2>JOBBO SINGLES</h2>
+      <div style={{ width: 400 }}>
+         
+              <p>
+                  <a href="https://drive.google.com/file/d/1wWsJdVWG5aSERwzpczXeB-Z--BeI1WcK/view?usp=sharing">SUITCASE REMIX</a>
+              </p>
+              <p>
+                  <a href="https://drive.google.com/file/d/16Li8IfgWENQpoSsuVgeuS-MY9mXSCwqe/view?usp=sharing">GRIND</a>
+              </p>
+              <p>
+                  <a href="https://drive.google.com/file/d/1aq06tCzJ26BDe1avDZYs1_F3EvqqeRz3/view?usp=sharing">HEARTBREAK</a>
+              </p>
+              <p>
+                  <a href="https://drive.google.com/file/d/1-4o-5IEHn2AE-hInN2Gs2427EFtzdC0S/view?usp=sharing">CHECK IT</a>
+              </p>
+              <p>
+                  <a href="https://drive.google.com/file/d/1ikhGZvqcjNkzwAz7NqxR52umZJCzrROG/view?usp=sharing">ORBIT</a>
+              </p>
+
+ 
+      </div>
+    </div>
+  )
+  }
+
+
+
+};
