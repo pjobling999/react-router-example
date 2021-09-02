f44d2339d63b6acfe5fbf1324a20f819f4ad7386
pjobling999 <62036028+pjobling999@users.noreply.github.com>
2021-09-02 09:06:14 +0100

    move to json files for tunes

--- src/App.js
+++ src/App.js
@@ -1,64 +1,33 @@
 import React from 'react'
+import JobboMixes from "./jobboMixes";
+import JobboSingles from "./jobboSingles";
 
 export class App extends React.Component {
-  render() { return (
+    
+    render() { 
+    
+    return (
+    
     <div>
       <h2>JOBBO MIXES</h2>
       <div style={{ width: 400 }}>
       
-              <p>
-                  <a href="https://drive.google.com/file/d/14CvBVPxKCiVOHLhYWg7wArDHpV5rFbQc/view?usp=sharing">SLAMMIN BEATZ 22</a>
-              </p>
-              <p>
-                  <a href="https://drive.google.com/file/d/1ueLS_UlzD-KDc1TzVPrjhuHtcCMagoaN/view?usp=sharing">SLAMMIN BEATZ 21</a>
-              </p>
-              <p>
-                  <a href="https://drive.google.com/file/d/1npCbEKN2Bf6_mbKr8n1P7_6WEIVTFPsK/view?usp=sharing">SLAMMIN BEATZ 20</a>
-              </p>
-              <p>
-                  <a href="https://drive.google.com/file/d/1I3Iq1_jswyItZ2cxh4lgdL7T9W1ucAHW/view?usp=sharing">SLAMMIN BEATZ 19</a>
-              </p>
-              <p>
-                  <a href="https://drive.google.com/file/d/19tKYgXIl0fnWUdaJ9lU2VOsR8nGRCsyc/view?usp=sharing">SLAMMIN BEATZ 18</a>
-              </p>
-              <p>
-                  <a href="https://drive.google.com/file/d/1b_3dgA4TaCwrHzXSer_0LB7PKgeEU_p4/view?usp=sharing">SLAMMIN BEATZ 17</a>
-              </p>
-              <p>
-                  <a href="https://drive.google.com/file/d/1712-tXtSUEN_bbugiFXCHsZh1Poxh7FD/view?usp=sharing">SLAMMIN BEATZ 16</a>
-              </p>
-              <p>
-                  <a href="https://drive.google.com/file/d/1__buFfpr0P0mWgtwhuGmyzfPwbRyu1en/view?usp=sharing">SLAMMIN BEATZ 15</a>
-              </p>
+              {JobboMixes.map((item, index) => {
+                    
+                    return <p key={index}><a href={item.href}>{item.Title}</a></p>;
+                        
+                })}
       </div>
 
       <h2>JOBBO SINGLES</h2>
       <div style={{ width: 400 }}>
          
-              <p>
-                  <a href="https://drive.google.com/file/d/1wWsJdVWG5aSERwzpczXeB-Z--BeI1WcK/view?usp=sharing">SUITCASE REMIX</a>
-              </p>
-              <p>
-                  <a href="https://drive.google.com/file/d/16Li8IfgWENQpoSsuVgeuS-MY9mXSCwqe/view?usp=sharing">GRIND</a>
-              </p>
-              <p>
-                  <a href="https://drive.google.com/file/d/1aq06tCzJ26BDe1avDZYs1_F3EvqqeRz3/view?usp=sharing">HEARTBREAK</a>
-              </p>
-              <p>
-                  <a href="https://drive.google.com/file/d/1-4o-5IEHn2AE-hInN2Gs2427EFtzdC0S/view?usp=sharing">CHECK IT</a>
-              </p>
-              <p>
-                  <a href="https://drive.google.com/file/d/1ikhGZvqcjNkzwAz7NqxR52umZJCzrROG/view?usp=sharing">ORBIT</a>
-              </p>
-              <p>
-                  <a href="https://drive.google.com/file/d/1itBLTZNmNSV8t8b1fOQXezhyAG-Ay6np/view?usp=sharing">THE WOBBLER</a>
-              </p>
-              <p>
-                  <a href="https://drive.google.com/file/d/1yUptmSDjUj-aIxt5XfLP5IOpQ5Q8PBNj/view?usp=sharing">BACK TO THE BASICS</a>
-              </p>
-              <p>
-                  <a href="https://drive.google.com/file/d/1WgTpw9XFDrddQsz6fuAzd9j51jBx2CCT/view?usp=sharing">KEY - FREE</a>
-              </p>
+              {JobboSingles.map((item, index) => {
+                    
+                    return <p key={index}><a href={item.href}>{item.Title}</a></p>;
+                        
+                })}
+
       </div>
     </div>
   )
