430cdcbb1ee07fb6c00edb1d0ef577b355ee7352
pjobling999 <62036028+pjobling999@users.noreply.github.com>
2021-09-02 17:30:41 +0100

    style it up

--- src/App.js
+++ src/App.js
@@ -1,34 +1,70 @@
 import React from 'react'
 import JobboMixes from "./jobboMixes";
 import JobboSingles from "./jobboSingles";
+import './App.module.css'; 
 
 export class App extends React.Component {
-    
+  
+    handleClick = (name) => {
+      window.location.href = name;
+    };
+  
     render() { 
     
     return (
     
     <div>
       <h2>JOBBO MIXES</h2>
-      <div style={{ width: 400 }}>
+      <div >
       
-              {JobboMixes.map((item, index) => {
-                    
-                    return <p key={index}><a href={item.href}>{item.Title}</a></p>;
-                        
-                })}
+        <ul className='tilesWrap'>
+          {JobboMixes.map((item, index) => {
+            
+            return (
+            
+              <li onClick={() => {this.handleClick(item.href);}} key={index}>
+                <h2>{index}</h2>
+                <h3>{item.Title}</h3>
+                <p>
+                
+                </p>
+                <button>Click to Play</button>
+              </li>
+              
+            )
+            
+          })}
+
+          </ul>
+
       </div>
 
       <h2>JOBBO TUNES AND JOBBO REMIXES</h2>
-      <div style={{ width: 400 }}>
+      <div >
          
-              {JobboSingles.map((item, index) => {
-                    
-                    return <p key={index}><a href={item.href}>{item.Title}</a></p>;
-                        
-                })}
+      <ul className='tilesWrap'>
+          {JobboSingles.map((item, index) => {
+            
+            return (
+            
+              <li onClick={() => {this.handleClick(item.href);}} key={index}>
+                <h2>{index}</h2>
+                <h3>{item.Title}</h3>
+                <p>
+                
+                </p>
+                <button>Click to Play</button>
+              </li>
+              
+            )
+            
+          })}
+
+          </ul>
 
       </div>
+
+      
     </div>
   )
   }
