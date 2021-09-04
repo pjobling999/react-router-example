23a306a42f748421344662bd015430c665e99461
pjobling999 <62036028+pjobling999@users.noreply.github.com>
2021-09-04 14:02:03 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import JobboMixes from "./jobboMixes";
-import JobboSingles from "./jobboSingles";
+import Jobbo from "./jobboStuff";
 import './App.module.css'; 
 
 export class App extends React.Component {
@@ -13,69 +12,52 @@ export class App extends React.Component {
     
     return (
     
-    <div>
       <div>
-        <header>
-          <h1 className="">THE TUNE ZONE</h1>
-        </header>
-      </div>
-
-      <main>
-        <p className="sectionHeaders">JOBBO MIXES</p>
-        <div >
-          
-          <ul className='tilesWrap'>
-            {JobboMixes.map((item, index) => {
-              
-              return (
-              
-                <li  key={index}>
-                  <h2>{index}</h2>
-                  <h3 ><a style={{color: "#b7b7b7"}} href={item.href}>{item.Title}</a></h3>
-                  <p>
-                  
-                  </p>
-                  <button onClick={() => {this.handleClick(item.href);}}>Click to Play</button>
-                </li>
-                
-              )
-              
-            })}
-
-            </ul>
-
+        <div>
+          <header>
+            <h1 className="">THE TUNE ZONE</h1>
+          </header>
         </div>
 
-        <p className="sectionHeaders">JOBBO ORIGINAL TUNES AND JOBBO REMIXES</p>
-        <div >
-          
-        <ul className='tilesWrap'>
-            {JobboSingles.map((item, index) => {
-              
-              return (
-              
-                <li  key={index}>
-                  <h2>{index}</h2>
-                  <h3 ><a style={{color: "#b7b7b7"}} href={item.href}>{item.Title}</a></h3>
-                  <p>
+        <main>
+
+        {Jobbo.map((topItem, i) => {
+
+          return (
+  
+          <div key={i}>
+            <div>
+              <p className="sectionHeaders">{topItem.category}</p>
+            
+              <ul className='tilesWrap'>
+
+                {topItem.tunes.map((item, index) => {
                   
-                  </p>
-                  <button onClick={() => {this.handleClick(item.href);}}>Click to Play</button>
-                </li>
-                
-              )
-              
-            })}
+                  return (
+                  
+                    <li  key={index}>
+                      <h2>{index}</h2>
+                      <h3 ><a style={{color: "#b7b7b7"}} href={item.href}>{item.Title}</a></h3>
+                      <p>
+                      
+                      </p>
+                      <button onClick={() => {this.handleClick(item.href);}}>Click to Play</button>
+                    </li>
+                    
+                  )
+                  })}
 
-            </ul>
+              </ul>
 
-        </div>
-      </main>
+            </div>
+          </div>
       
-    </div>
-  )
+        )
+        })}
+
+        </main>
+      </div>
+    )
   }
-
-
 
 };
