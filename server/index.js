2c91538e6e224251b0b89a8faffd7b69249054b4
pjobling999 <62036028+pjobling999@users.noreply.github.com>
2022-02-08 16:28:09 +0000

    debug

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ import fetch from 'node-fetch';
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+function rejected(result) {
+    console.error(result);
+}
+
 app.get("/api/:id/:title", (req, res) => {
 
     var id = req.params.id;
@@ -13,34 +17,19 @@ app.get("/api/:id/:title", (req, res) => {
     var key = process.env.GOOGLE_API_KEY;
 
     fetch(`https://www.googleapis.com/drive/v3/files/${id}?alt=media&key=${key}`).then(res2 => new Promise((resolve, reject) => {
-        const dest = fs.createWriteStream('./tmp/download.mp3');
+        const dest = fs.createWriteStream('download.mp3');
         res2.body.pipe(dest);
         dest.on('close', () => resolve(dest));
-        dest.on('error', reject);
+        dest.on('error', rejected);
     })).then(x => { 
             
             res.attachment(title + '.mp3');
-            var readStream = fs.createReadStream('./tmp/download.mp3');
+            var readStream = fs.createReadStream('download.mp3');
             readStream.pipe(res);
         });
 
 
 });
-
-app.get('/', (req, res) => {
-
-    fetch(`https://www.googleapis.com/drive/v3/files/1hw5OeRDcJSxizFaqYGr07inLuKk68oRF?alt=media&key=${process.env.GOOGLE_API_KEY}`).then(res2 => new Promise((resolve, reject) => {
-        const dest = fs.createWriteStream('./tmp/download.mp3');
-        res2.body.pipe(dest);
-        dest.on('close', () => resolve());
-        dest.on('error', reject);
-    })).then(x => { 
-            
-        res.send(process.env.GOOGLE_API_KEY).end();
-    });
-    
-
-  });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
