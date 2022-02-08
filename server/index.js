7be2c0250045548ceee3087d33ca85f2a8166d12
pjobling999 <62036028+pjobling999@users.noreply.github.com>
2022-02-08 16:35:48 +0000

    debug

--- server/index.js
+++ server/index.js
@@ -17,14 +17,14 @@ app.get("/api/:id/:title", (req, res) => {
     var key = process.env.GOOGLE_API_KEY;
 
     fetch(`https://www.googleapis.com/drive/v3/files/${id}?alt=media&key=${key}`).then(res2 => new Promise((resolve, reject) => {
-        const dest = fs.createWriteStream('download.mp3');
+        const dest = fs.createWriteStream('/tmp/download.mp3');
         res2.body.pipe(dest);
         dest.on('close', () => resolve(dest));
         dest.on('error', rejected);
     })).then(x => { 
             
             res.attachment(title + '.mp3');
-            var readStream = fs.createReadStream('download.mp3');
+            var readStream = fs.createReadStream('/tmp/download.mp3');
             readStream.pipe(res);
         });
 
