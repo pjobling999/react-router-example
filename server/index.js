5611aee0f02659a848c1dad27065ea935639b11e
pjobling999 <62036028+pjobling999@users.noreply.github.com>
2022-02-09 17:14:12 +0000

    try

--- server/index.js
+++ server/index.js
@@ -13,36 +13,33 @@ const downloadFile = (async (url, path) => {
     const res = await fetch(url);
     const dest = fs.createWriteStream(path);
     await new Promise((resolve, reject) => {
-        
        
         res.body.pipe(dest);
         res.body.on("error", reject);
         dest.on('finish', resolve);
 
-    }).then(x => console.log('fetch complete') );
+    });
 
   });
 
 
-app.get("/api/:id/:title", (req, res) => {
+app.get("/api/:id/:title", async (req, res) => {
 
     var id = req.params.id;
     var title = req.params.title;
     var key = process.env.GOOGLE_API_KEY;
     var url = `https://www.googleapis.com/drive/v3/files/${id}?alt=media&key=${key}`;
-    var path = '/tmp/download.mp3';
+    var path = 'tmp/download.mp3';
 
     console.log(url);
     
-    downloadFile(url,path);
-    console.log('about to download')
-    res.download(path,title + '.mp3');
-    
+    await downloadFile(url, path);
+
+    console.log('About to download');
+    res.download(path, title + '.mp3');
 });
 
-app.get("/api", (req, res) => {
-    console.log(`GET TEST`);
-});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
